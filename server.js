1cfdd3f78bb4243870fb2d5076b074262d9ba423
angelinaa-ligo <maguerts@gmail.com>
2024-11-30 20:22:12 -0500

    commit

--- server.js
+++ server.js
@@ -188,3 +188,53 @@ app.post("/login", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+const { Server } = require('socket.io');
+const http = require('http')
+
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+})
+
+
+
+client.connect().then(() => {
+  console.log("Connected to MongoDB");
+  const db = client.db("quizApp"); 
+  quizzesCollection = db.collection("quizzes"); 
+  usersCollection = db.collection("users");
+});
+
+//Checking if the quizz code is valid
+io.on("connection", (socket) => {
+    console.log(`User is connected: ${socket.id}`);
+  
+    socket.on("send_code", async (code) => {
+      console.log("Received quiz code:", code);
+      try {
+        const quiz = await quizzesCollection.findOne( {quizId: Number(code)} );
+    
+        if (quiz && quiz.isValid) {
+          socket.emit('checkQuizCode', { isValid: true });
+        } else {
+          socket.emit('checkQuizCode', { isValid: false });
+        }
+      } catch (err) {
+        console.error("Error querying database:", err);
+        socket.emit('checkQuizCode', { isValid: false });
+      }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+      });
+})
+
+server.listen(3001, () => {
+    console.log(`Socket server 3001 is running!`)
+})
